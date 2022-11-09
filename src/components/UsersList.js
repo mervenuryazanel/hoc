44458259d6e4ac2951e1558a05a1d815b98a9c9a
merve nur yazanel <mervenuryazanel@gmail.com>
2022-11-09 14:20:19 +0300

    initial commit

---
+++ src/components/UsersList.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+const UsersList = () => {
+    const [users, setUsers] = useState([]);
+    const [term, setTerm] = useState("");
+    useEffect(
+        () => {
+            const fetchUsers = async () => {
+                const res = await fetch("https://jsonplaceholder.typicode.com/users");
+                const json = await res.json();
+                setUsers(json);
+                console.log("users", json);
+            }
+
+            fetchUsers();
+        }, [])
+
+    let renderUsers = users.map((user) => {
+        return (
+            <div key={user.id}>
+                <p>
+                    <strong> {user.name}</strong>
+                </p>
+            </div>
+        )
+    })
+
+    let filteredUsers = users.filter(({ name })=> {
+        return name.indexOf(term) >= 0;
+    })
+        .map((user) => {
+            return (
+                <div key={user.id}>
+                    <p>
+                        <strong>{user.name}</strong>
+                    </p>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            {/* <h2>
+                Users List
+            </h2>
+            <input type="text" value={term} onChange={(e)=>{ setTerm(e.target.value)}}>
+            </input>
+            {renderUsers} */}
+
+            <h2>
+                Filtered Users List
+            </h2>
+            <input type="text" value={term} onChange={(e) => { setTerm(e.target.value) }}>
+            </input>
+            {filteredUsers}
+        </div>
+    )
+
+}
+
+export default UsersList;
